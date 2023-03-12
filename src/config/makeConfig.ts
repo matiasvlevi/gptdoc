4a72f9418934d85659c72ea00764276aeb9a03c6
matiasvlevi <matiasvlevi@gmail.com>
2023-03-12 11:54:37 +0000

    Replaced `priorize` with ?? operator
    * `priorize` did exactly what `??` does for boolean values with a possblie
      undefined state

--- src/config/makeConfig.ts
+++ src/config/makeConfig.ts
@@ -2,22 +2,6 @@ import { Config } from './types'
 import { CLIArgs } from "../cli";
 import parseEnv from '../utils/dotenv';
 
-/**
- * Takes the first non-undefined value
- * Used mostly for boolean values
- * 
- * @param arr Array of primitives
- * @returns 
- */
-function priorize(arr: any[]) {
-    for (let i = 0; i < arr.length; i++) {
-
-        if (arr[i] !== undefined)
-            return arr[i];
-    }
-    return arr[arr.length-1];
-}
-
 /** 
  * Merge a configuration with the defaults
  * 
@@ -35,13 +19,13 @@ export function makeConfig(
         _config.files = {};
 
     return {
-        DEBUG: priorize([cli_arg.options.production,_config.DEBUG, false]),
-        disableHeader: priorize([_config.disableHeader, false]),
+        DEBUG: cli_arg.options.production ?? _config.DEBUG ?? false,
+        disableHeader: _config.disableHeader ?? false,
         tab_size: _config.tab_size || 4,
         framework: _config.framework || 'JSDOC',
         language: _config.language || 'JS',
         prompt: _config.prompt || '',
-        minify: _config.minify || true,
+        minify: _config.minify ?? true,
         openai: {
             temperature: _config.openai.temperature || 0.75,
             top_p: _config.openai.top_p || 1,
@@ -51,7 +35,7 @@ export function makeConfig(
         files: {
             src: cli_arg.src || _config.files.src || './src',
             dest: cli_arg.dest || _config.files.dest || './generated-doc',
-            recursive: priorize([cli_arg.options.recursive, _config.files.recursive, false])
+            recursive: cli_arg.options.recursive ?? _config.files.recursive ?? false
         },
         apiKey: process.env.OPENAI_API_KEY || parseEnv('.env').OPENAI_API_KEY || ''
     }
